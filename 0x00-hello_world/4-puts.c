3acf04787dec7ed0d3175351ea576cbcb9bc0e62
Uzor <uzorgodsboy1@gmail.com>
2023-02-16 12:57:24 -0800

    puzzles

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
  * main - Entry point
- * return: 0 after printing the function
+ *
+ * Return: Alway 0 (Success)
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzles");
 	return (0);
 }
