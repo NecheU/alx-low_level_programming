52a266c985ec1a352c047166ec883fcf385fe8a7
Uzor <uzorgodsboy1@gmail.com>
2023-02-17 13:19:15 -0800

    low & uppercase alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+low char;
+for (low = 'a'; low <= 'z'; low++)
+putchar(low);
+putchar(low = 'A'; low <= 'Z'; low++)
+putchar(low);
+putchar('\n');
+
+return (0);
+}
