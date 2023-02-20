da6d9165f917bd42ebfded5b2a7c8ee62548ec40
Uzor <uzorgodsboy1@gmail.com>
2023-02-20 14:10:41 -0800

    task 100

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -3,7 +3,7 @@
 #include <time.h>
 
 /**
- * main - Entry point
+ * main - Entry point 
  *
  * Return: Always 0 (Success)
  */
@@ -12,11 +12,11 @@ int main(void)
 {
 int d, p;
 
-for (d = '0'; d < '9'; d++)
+for (d = '0'; d < '9': d++)
 {
-for (p = d + 1; p <= '9'; p++)
+for (p = d + 1; d <= '9' p++)
 {
-for (p != d)
+if (p != d)
 {
 putchar(d);
 putchar(p);
