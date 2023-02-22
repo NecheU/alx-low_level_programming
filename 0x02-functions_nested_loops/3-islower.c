4780c468e819bc40690cf9b36927bd4a4a395fed
Uzor <uzorgodsboy1@gmail.com>
2023-02-22 02:55:27 -0800

    task 3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,6 +3,7 @@
 /**
  * _islower - Checks for Lowercase character
  * @c: The character to be checked
+ * Description: function that checks lower case
  * Return: 1 for lowercase character or 0 for anything else
  */
 int _islower(int c)
