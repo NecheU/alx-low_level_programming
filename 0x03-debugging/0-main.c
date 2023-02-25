ddf8dd16aae45804c1473758b203fa7767a19fe1
Uzor <uzorgodsboy1@gmail.com>
2023-02-24 18:29:18 -0800

    0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,6 +1,7 @@
 #include "main.h"
+
 /**
- * main - main
+ * main - tests function that prints if integer is positive or negative
  * Return: 0
  */
 
@@ -8,8 +9,7 @@ int main(void)
 {
 	int i;
 
-	i = 0;
-
+	i = 98 % 2;
 	positive_or_negative(i);
 
 	return (0);
