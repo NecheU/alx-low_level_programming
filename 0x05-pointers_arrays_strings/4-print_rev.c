4fa15b6b2399fc56dadbc77da2fc4beb36396622
Uzor <uzorgodsboy1@gmail.com>
2023-02-27 15:14:13 -0800

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/*
+ * print_rev - string on reverse
+ * @s: string
+ * Return: 0
+ */
+void print_rev(char *s)
+{
+	int rev = 0;
+	int f;
+
+	while (*s != '\0')
+	{
+		rev++;
+		s++;
+
+	}
+	s--;
+	for (f = rev; f > 0; f--)
+	{
+		_putchar(*s);
+		s--;
+
+	}
+}
+
