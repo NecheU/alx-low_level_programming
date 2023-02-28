6a20e56d5fd7076d793aa0b929a510e728be93a3
Uzor <uzorgodsboy1@gmail.com>
2023-02-27 16:06:41 -0800

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -7,23 +7,23 @@
  */
 void puts2(char *str)
 {
-	 int longi = 0;
-	 int f = 0;
-	 char *y = str;
-	 int d;
+	int longi = 0;
+	int f = 0;
+	char *y = str;
+	int d;
 
-	 while (*y != '\0')
-	  {
-		  y++;
-		  longi++;
-	  }
-	  f = longi - 1;
-	  for (d = 0 ; d <= f ; d++)
-	  {
-		  if (d % 2 == 0)
-		  {
-			  _putchar(str[d]);
-		  }
-	  }
-	  _putchar('\n');
+	while (*y != '\0')
+	{
+	y++;
+	longi++;
+	}
+	f = longi - 1;
+	for (d = 0 ; d <= f ; d++)
+	{
+	if (d % 2 == 0)
+	{
+	_putchar(str[d]);
+	}
+	}
+	_putchar('\n');
 }
