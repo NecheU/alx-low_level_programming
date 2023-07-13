c33792604cd700b994b9aaa776aa4e388fc049f6
Uzor <uzorgodsboy1@gmail.com>
2023-07-13 06:04:14 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 
 /**
- * Entry point
- * Descrpition: Print strings
- * Return: Always Success 0
+ * main - Entry
+ * Return: Always 0
  */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return 0;
+	return (0);
 }
