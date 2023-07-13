2b1b1b10bdef4a8d045d4887e26f4de85a988392
Uzor <uzorgodsboy1@gmail.com>
2023-07-13 06:26:26 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry 
+ * Return: Always 0
+ */
+int main(void)
+{
+	printf("The size of a char: %lu bytes \n", sizeof(char));
+	printf("The size of int: %lu bytes \n", sizeof(int));
+	printf("The size of a long int: %lu bytes \n", sizeof(long int));
+	printf("The size of a long long int: %lu bytes \n", sizeof(long long int));
+	printf("The size of a float: %lu bytes \n", sizeof(float));
+	return (0);
+}
