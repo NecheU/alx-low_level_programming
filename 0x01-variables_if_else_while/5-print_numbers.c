fbf2dbcca211e39fc5cbfe13c23e7c0e3b239738
Uzor <uzorgodsboy1@gmail.com>
2023-07-15 03:53:48 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - C program to print base 10 bumbers from 0 to 10
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num <= 10; num++)
+	{
+		printf("%d\n", num);
+	}
+	return (0);
+}
