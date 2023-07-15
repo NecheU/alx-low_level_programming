13c1dd426436047391cf0ffe5a11043db2181270
Uzor <uzorgodsboy1@gmail.com>
2023-07-15 04:29:40 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - C program to print num 0 to 10 with only putchar
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int no_char;
+
+	for (no_char = 0; no_char < 10; no_char++)
+	{
+		putchar(no_char);
+	}
+	putchar('\n');
+	return (0);
+}
