d792f52c2d620602eebe138b8eb34cfb9286d9a0
Uzor <uzorgodsboy1@gmail.com>
2023-07-16 07:21:01 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - C program to print all possible
+ *  combination of two digits
+ *
+ *  Return: 0
+ */
+int main(void)
+{
+	int u, v;
+
+	for (u = 0; u < 9; u++)
+	{
+		for (v = u + 1; v < 10; v++)
+		{
+			putchar((u % 10) + '0');
+			putchar((v % 10) + '0');
+			if (u == '8' && v == '9')
+				continue;
+		}
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
