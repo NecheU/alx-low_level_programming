f49339f9a46b8dd234d7709764152d83f4d94010
Uzor <uzorgodsboy1@gmail.com>
2023-07-16 08:02:34 -0700

    101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main - C programe to print combination
+ * of three (3) numbers
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int u, v, w;
+
+	for (u = 0; u < 9; u++)
+	{
+		for (v = u + 1; v < 9; v++)
+		{
+			for (w = v + 1; w < 10; w++)
+			{
+				putchar((u % 10) + '0');
+				putchar((v % 10) + '0');
+				putchar((w % 10) + '0');
+				if (u == 8 && v == 9 && w == 10)
+					continue;
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
