41938b60326ad8f9aaa1cd7115d9505930f29d39
Uzor <uzorgodsboy1@gmail.com>
2023-07-16 06:20:22 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - program that prints combination of aingle numbers
+ *
+ * Retuen: 0
+ */
+int main(void)
+{
+	int comb;
+
+	for (comb = '0'; comb <= '9'; comb++)
+	{
+		putchar(comb);
+		if (comb == '9')
+		continue;
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
