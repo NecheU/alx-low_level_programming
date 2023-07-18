fee2c17b20958a926f3a60d9db0a895319d2c2c6
Uzor <uzorgodsboy1@gmail.com>
2023-07-18 12:11:17 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - C program that prints "_putchar"
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchae('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
