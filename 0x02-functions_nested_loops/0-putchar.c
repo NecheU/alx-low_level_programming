8a00788ec5d8de131c14734c195e03d2805a56ed
Uzor <uzorgodsboy1@gmail.com>
2023-07-18 13:38:34 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,16 +4,17 @@
  *
  * Return: 0
  */
-int main(void)
+int _putchar(char);
 {
 	_putchar('_');
 	_putchar('p');
 	_putchar('u');
 	_putchar('t');
-	_putchae('c');
+	_putchar('c');
 	_putchar('h');
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
+
 	return (0);
 }
