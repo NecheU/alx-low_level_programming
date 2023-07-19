4e240c1ba7f0cd3276868c5866c19cb468700e55
Uzor <uzorgodsboy1@gmail.com>
2023-07-19 06:40:08 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _abs - prints tve absolute value of a num
+ * @i: the number to be evaluated
+ *
+ * Return: 1,0 or -1
+ */
+int _abs(int i)
+{
+	if (i < 0)
+	return (-1);
+	else if (i > 0)
+		return (1);
+	else
+		return (0);
+}
