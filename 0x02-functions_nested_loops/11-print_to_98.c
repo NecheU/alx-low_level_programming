871fbbf29a66936001da9124216408b65c769707
Uzor <uzorgodsboy1@gmail.com>
2023-07-19 13:58:51 -0700

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -3,25 +3,21 @@
 /**
  * print_to_98 - prints natural numbers
  * from n to 98
- *
+ * @n: the number to start counting from
  * Return: nothing
  */
 void print_to_98(int n)
 {
 	if (n >= 98)
 	{
-		while 
-			(n > 98)
+		while (n > 98)
 			printf("%d, ", n--);
-
-			printf("%d\n", n);
+		printf("%d\n", n);
 	}
 	else
 	{
-		while 
-			(n <= 98)
+		while (n < 98)
 			printf("%d, ", n++);
-
-			printf("%d\n", n);
+		printf("%d\n", n);
 	}
 }
