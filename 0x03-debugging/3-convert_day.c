060e673f4f789071fcabbd7bface365b9d28b89c
Uzor <uzorgodsboy1@gmail.com>
2023-07-21 07:33:24 -0700

    3-convert_day.c

--- 0x03-debugging/3-convert_day.c
+++ 0x03-debugging/3-convert_day.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
 * convert_day - converts day of the month to day of the year
-* without considering the leap year
+* without accounting for leap year
 * @month: month in number format
 * @day: day of month
 * Return: day of year
