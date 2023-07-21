da242b3bea758d28ee75efddf1211ab437dccb9a
Uzor <uzorgodsboy1@gmail.com>
2023-07-21 04:09:18 -0700

    0-main.c

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - tests function that prints positive or
+ * negative number
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int a;
+
+	a = 0;
+
+	positive_or_negative(a);
+
+	return (0);
+}
