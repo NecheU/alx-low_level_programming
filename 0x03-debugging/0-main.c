ffc6c1e6b192e9e8a24f6bc454f899660a0ffb6c
Uzor <uzorgodsboy1@gmail.com>
2023-07-21 06:55:43 -0700

    0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,7 +1,6 @@
 #include "main.h"
 /**
- * main - tests function that prints positive or
- * negative number
+ * main - Test function that prints if integer is positive or negative
  *
  * Return: 0
  */
