dad207a3925c79287b41842d49328a388921bc74
Uzor <uzorgodsboy1@gmail.com>
2023-07-21 15:23:51 -0700

    7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -13,7 +13,7 @@ void print_diagonal(int n)
 	{
 	for (len = 0; len < n; len++)
 	{
-	for (space = 0; space < n; space++)
+	for (space = 0; space < len; space++)
 	_putchar(' ');
 
 	_putchar('\\');
