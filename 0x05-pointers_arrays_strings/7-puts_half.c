10761db60d8be8f39cc1b05c6bccf370817d275f
Uzor <uzorgodsboy1@gmail.com>
2023-07-24 12:20:41 -0700

    7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * puts_half - prints half of a string
+ * @str: the string
+ *
+ * Return: void
+ */
+void puts_half(char *str)
+{
+	int half = 0, i;
+
+	while (half >= 0)
+	{
+		if (str[half] != '\0')
+			break;
+		half++;
+	}
+	if (half % 2 == 1)
+		i = half / 2;
+	else
+		i = (half - 1) / 2;
+	for (i++; i < half; i++)
+		_putchar(str[i]);
+	_putchar('\n');
+}
