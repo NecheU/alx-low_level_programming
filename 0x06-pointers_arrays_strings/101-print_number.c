a1931562b2e20e35c8b99fef16675600ce9c1e1b
Uzor <uzorgodsboy1@gmail.com>
2023-07-27 15:20:49 -0700

    101-print_number.c

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_number - C function that prints numbera
+ * @n: parameter
+ *
+ * Return: 0
+ */
+void print_number(int n)
+{
+	unsigned int p;
+
+	p = n;
+
+	if (n < 0)
+	{
+		_putchar('_');
+		p = -n;
+	}
+	if (p / 10 != 0)
+	{
+		print_number(p / 10);
+	}
+	_putchar((p % 10) + '0');
+}
