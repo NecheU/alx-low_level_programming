7293342e2060d3dba3b925701ea5a7320981a8e2
Uzor <uzorgodsboy1@gmail.com>
2023-07-28 16:40:24 -0700

    7-leet.c

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * leet - encodes a string using leet encoding
+ * @m: string to be encoded
+ *
+ * Return: encoded string
+ */
+char *leet(char *m)
+{
+	int y, j;
+	char *t = "aAeEoOtTlL";
+	char *v = " 4433007711";
+
+	for (y = 0; m[y] != '\0'; y++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if (m[y] == t[j])
+			break;
+		}
+	}
+	return (m);
+}
