607c1b1be5fb5d807e95e7ee65e70f732d8c57f8
Uzor <uzorgodsboy1@gmail.com>
2023-08-03 16:17:47 -0700

    6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -8,23 +8,23 @@
  */
 int is_prime_number(int n)
 {
-	int start = n / 2;
+	int first = n / 2;
 
 	if (n <= 1)
 		return (0);
-	return (is_prime(n, start));
+	return (is_prime(n, first));
 }
 /**
  * is_prime- Fuction that returns 1 if a number is prime
  * @n: the number to be checkd
- *
+ * @first: parameter
  * Return: 1 if n is prime, 0 if otherwise
  */
-int is_prime(int n, int start)
+int is_prime(int n, int first)
 {
-	if (n <= 1)
+	if (first <= 1)
 		return (1);
-	else if (n % start == 0)
+	else if (n % first == 0)
 		return (0);
-	return (is_prime(n, start - 1));
+	return (is_prime(n, first - 1));
 }
