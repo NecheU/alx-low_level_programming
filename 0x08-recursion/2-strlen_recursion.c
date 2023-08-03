07de8c457ada68bba67e83c4967df563f2c59842
Uzor <uzorgodsboy1@gmail.com>
2023-08-03 15:03:47 -0700

    2-strlen_recursion.c

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strlen_recursion - Prints rhe length of a string
+ * @s: the sting to be measured
+ *
+ * Return: lengrh of the string
+ */
+int _strlen_recursion(char *s)
+{
+	int hue = 0;
+
+	if (*s != '\0')
+	{
+		hue++;
+		hue += _strlen_recursion(s + 1);
+	}
+	return (hue);
+}
