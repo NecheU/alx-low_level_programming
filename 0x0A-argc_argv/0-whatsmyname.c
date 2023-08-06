0d074541816d178b9be6bbc5602f6977de0db233
Uzor <uzorgodsboy1@gmail.com>
2023-08-06 10:57:19 -0700

    my_name_is

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - C program that prints its name
+ * @argc: arguement count
+ * @argv: arguement vector
+ *
+ * Return: 0
+ */
+int main(int __attribute__ ((__unused__)) argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
