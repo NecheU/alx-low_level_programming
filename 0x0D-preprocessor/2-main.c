26dd6c7e6f85f48e1a7888bcc3dca1f753ab6b95
Uzor <uzorgodsboy1@gmail.com>
2023-08-14 05:32:34 -0700

    2-main.c

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - C program that prints the name of a file
+ * it was compiled from
+ *
+ * Return: nothing
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
