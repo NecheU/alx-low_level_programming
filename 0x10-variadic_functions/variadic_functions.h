a35551261bf212232b2c208804d342f7a65ccc48
Uzor <uzorgodsboy1@gmail.com>
2023-08-17 11:27:37 -0700

    variadic_functions.h

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -19,5 +19,7 @@ void print_int(va_list arg);
 void print_float(va_list arg);
 void print_string(va_list arg);
 void print_all(const char * const format, ...);
+int _putchar(char c);
+
 
 #endif /*VARIADIC_FUNCTION*/
