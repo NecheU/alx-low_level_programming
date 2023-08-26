3099d9de602a81759f81671428a5975af129191e
Uzor <uzorgodsboy1@gmail.com>
2023-08-26 05:11:33 -0700

    103-fibonacci.c

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -7,17 +7,17 @@
  */
 int main(void)
 {
-	unsigned long n1 = 0; n2 = 1; n3 = 0; sum = 0;
+	unsigned long a = 0; b = 1; c = 0; sum = 0;
 
-	while (n3 <= 4000000)
+	while (c <= 4000000)
 	{
-		n3 = n1 + n2;
-		n1 = n2;
-		n2 = n3;
+		c = a + b;
+		a = b;
+		b = c;
 
-		if ((n1 % 2) == 0)
-			sum += n1;
+		if ((a % 2) == 0)
+			sum += a;
 	}
-	printf("%ld\n", sum);
+	printf("%lu\n", sum);
 	return (0);
 }
