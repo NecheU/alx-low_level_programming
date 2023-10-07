dee4699448562dc391ac3367e6b4c3e608819d9e
Uzor <uzorgodsboy1@gmail.com>
2023-10-07 09:12:17 -0700

    2-calloc.c

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _calloc - C funtion that allocates memory for
+ * an areay, using malloc
+ * @size: size of the memory to be allocated
+ * @nmemb: member of the elments
+ *
+ * Return: pointer to the allocated memory or NULL if failure
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	int *memb;
+	unsigned int c;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	memb = malloc(size * nmemb);
+	if (memb != NULL)
+	{
+		for (c = 0; c < (size * nmemb); c++)
+			memb[c] = 0;
+	}
+	else
+	{
+		return (NULL);
+	}
+}
