9e66bad451e33dbbfc5d860cbdf8f3565f63cb5c
Uzor <uzorgodsboy1@gmail.com>
2023-10-10 13:38:35 -0700

    4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -70,17 +70,17 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	dog->name = malloc(sizeof(char) * (c + 1));
 	{
-	if (dog->name == NULL)
-		free(dog);
-	return (NULL);
+		if (dog->name == NULL)
+			free(dog);
+		return (NULL);
 	}
 
 	dog->owner = malloc(sizeof(char) * (d + 1));
 	{
-	if (dog->owner == NULL)
-		free(dog);
+		if (dog->owner == NULL)
+			free(dog);
 		free(dog->name);
-	return (NULL);
+		return (NULL);
 	}
 
 	_strcpy(dog->name, name);
