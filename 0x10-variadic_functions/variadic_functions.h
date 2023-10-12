8989794fe7ecd2c311da89b5d54a62c462ab10b8
Uzor <uzorgodsboy1@gmail.com>
2023-10-12 14:31:38 -0700

    variadic_functions.h

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,6 +1,9 @@
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
 
+#include <stdlib.h>
+#include <string.h>
+#include <typedef.h>
 #include <stdarg.h>
 #include <stdio.h>
 
@@ -10,6 +13,7 @@ typedef struct printer{
 } printer_t;
 
 
+int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
