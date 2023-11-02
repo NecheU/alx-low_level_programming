1407e5b2ae5bb723c891f5341d2d05291a44dcd2
Uzor <uzorgodsboy1@gmail.com>
2023-11-02 03:24:53 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdlib.h>
 
 /**
  * create_file - C funtion that creates a file
@@ -15,7 +14,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	file = open(filename, O_WRONLY | O_CREAT, O_TRUNC, 0600);
+	file = open(filename, O_CREAT | O_RDWR |  O_TRUNC, 0600);
 	if (file == -1)
 		return (-1);
 
