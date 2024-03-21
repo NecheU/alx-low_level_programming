16dabaa492c087715a4fab2d5fdd8197bde5d250
Uzor <uzorgodsboy1@gmail.com>
2024-03-21 15:26:33 -0700

    1-dlistint_len.c

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - C funtction to returns the number of element in
+ * a linked list
+ * @h: head to the link list
+ *
+ * Return: Always success
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t node_counter = 0;
+
+	while (h != NULL) 
+	{
+		node_counter++;
+		h = h->next;
+	}
+
+	return node_counter;
+}
